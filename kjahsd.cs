4f6a09cca5cabcdd389e71b5609793df5d7c18de
Willrath47 <99066195+Willrath47@users.noreply.github.com>
2022-05-22 07:56:12 -0600

    Create kjahsd.cs

---
+++ kjahsd.cs
@@ -0,0 +1,21 @@
+
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Console.WriteLine("Si es editable");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
+
+
+
+
